537fd4cdd88d7c83f9a488fbc6a9b8bee38f8e52
dalonamorgan <82848765+dalonamorgan@users.noreply.github.com>
2021-05-05 19:20:39 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,56 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Enter Grade varying from A,B,C,D,failed"); 
+
+        String grade1 = "A";
+        String grade2 = "B";
+        String grade3 = "C";
+        String grade4 = "failed";
+        
+        if (grade1 == "A") { 
+
+            System.out.println("Bryan, Alex, Ben and Shelby has scored between 90 to 100"); 
+            
+
+        }
+      
+          if (grade2 == "B") { 
+
+            System.out.println("Toni and Chris has scored between 80 to 89"); 
+
+        } 
+
+       
+
+       if (grade3 == "C") { 
+
+            System.out.println("Tyler and Tiffany has scored between 70 to 79"); 
+
+        } 
+
+        
+
+         if (grade4 == "failed") { 
+
+            System.out.println("Sarah and Erin has scored between 55 to 69"); 
+
+        } 
+
+              } 
+
+     
+
+        
+
+    } 
+
+
+ 	
+
+
+
+	
+
+
